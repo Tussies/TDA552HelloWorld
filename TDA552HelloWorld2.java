aa7b66b92b5658017e27c7a1111bc9e14408e083
elzorroartico <gilabert.alexander@gmail.com>
2021-11-03 16:22:19 +0100

    En bra fil

--- TDA552HelloWorld2.java
+++ TDA552HelloWorld2.java
@@ -1,3 +1,6 @@
 public static void main(String[] args){
     System.out.println("Goodbye you son of a gun")
+        for i = 0; i < 10 ; i++{
+            System.out.print(" " + i)
+        }
     }
