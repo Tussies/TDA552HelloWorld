97a5bcaed30af54a8e34e13c38ce508310e473f5
Annheij <ann.heijkenskjold@gmail.com>
2021-11-03 16:49:11 +0100

    nya changes

--- TDA552HelloWorld2.java
+++
@@ -1,8 +0,0 @@
-public static void main(String[] args){
-    System.out.println("OOp")
-        for i = 0; i < 10 ; i++{
-            System.out.print(" " + i)
-        }
-    System.out.println("hjhhkhkhjkh")
-
-    }
---
+++ src/TDA552HelloWorld2.java
@@ -0,0 +1,12 @@
+
+
+public class TDA552HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("OOp");
+        for (int i = 0; i< 10; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println("hjhhkhkhjkh");
+
+    }
+}
