6765ec1aa22b2a643ae0c8b9b98fa1cc402621d8
Annheij <ann.heijkenskjold@gmail.com>
2021-11-04 13:27:32 +0100

    nya changes

--- src/TDA552HelloWorld2.java
+++ src/TDA552HelloWorld2.java
@@ -3,8 +3,8 @@
 public class TDA552HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("OOp");
-        System.out.println("Lista av olika numrerade Hanna som Alex gillar:");
-        for (int i = 0; i< 10; i++) {
+        System.out.println("Uppräkning av olika Hanna på Vino som Alex gillar:");
+        for (int i = 1; i< 21; i++) {
             System.out.print(" " + i);
         }
 
